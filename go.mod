b8b9d073a349167ac38bdcc8c86b9409a4a9ce4d
pultzlucas <lucas.pultz@nappsolutions.com>
2024-04-30 17:29:56 -0300

    feat: extracting categories and products lins

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module golang-crawler
 go 1.19
 
 require (
-	github.com/anaskhan96/soup v1.2.5 // indirect
-	github.com/ledongthuc/goterators v1.0.2 // indirect
+	github.com/anaskhan96/soup v1.2.5
+	github.com/ledongthuc/goterators v1.0.2
+	golang.org/x/net v0.24.0
+)
+
+require (
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
-	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,65 +1,122 @@
 package main
 
 import (
-	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
-	"os"
+	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/anaskhan96/soup"
 	"github.com/ledongthuc/goterators"
 )
 
-const BASE_URL = "https://pokemondb.net/pokedex/national"
+const BASE_URL = "https://sinete.com.br"
 
-type Pokemon struct {
-	id    string
-	name  string
-	types []string
-	image string
+type Category struct {
+	url string
+}
+
+type Product struct {
+	sku         string
+	ean         string
+	name        string
+	description string
+	images      []string
+	brand       string
+	factory     string
+}
+
+func getCategories(doc soup.Root) []Category {
+	allCategoriesElem := doc.FindAll("a", "class", "menu-link")
+	allCategoriesLink := goterators.Map(allCategoriesElem, func(item soup.Root) Category {
+		return Category{url: item.Attrs()["href"]}
+	})
+	return goterators.Filter(allCategoriesLink, func(item Category) bool {
+		return item.url != ""
+	})
+}
+
+func getProductsLinkFromCategory(category Category) []string {
+	page := 1
+
+	var allProductsLink []string
+
+	for {
+		client := &http.Client{}
+		var data = strings.NewReader(`ajax_nav=1`)
+		req, err := http.NewRequest("POST", category.url+"?p="+strconv.Itoa(page), data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Header.Set("accept", "*/*")
+		req.Header.Set("accept-language", "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7")
+		req.Header.Set("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
+		req.Header.Set("cookie", "PHPSESSID=a60d7a683a4f06038c1a0cd9546e577c; form_key=gIbYSYa3BgwEVmMd; form_key=gIbYSYa3BgwEVmMd; mage-messages=; cookies-policy=1; private_content_version=913083a594f90562bc29be3ff5648957; mage-cache-storage=%7B%7D; mage-cache-storage-section-invalidation=%7B%7D; mage-cache-sessid=true; section_data_ids=%7B%22cart%22%3A1714505382%7D; recently_viewed_product=%7B%7D; recently_viewed_product_previous=%7B%7D; recently_compared_product=%7B%7D; recently_compared_product_previous=%7B%7D; product_data_storage=%7B%7D")
+		req.Header.Set("dnt", "1")
+		req.Header.Set("origin", "https://sinete.com.br")
+		req.Header.Set("referer", "https://sinete.com.br/medicamentos.html")
+		req.Header.Set("sec-ch-ua", `"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"`)
+		req.Header.Set("sec-ch-ua-mobile", "?0")
+		req.Header.Set("sec-ch-ua-platform", `"Linux"`)
+		req.Header.Set("sec-fetch-dest", "empty")
+		req.Header.Set("sec-fetch-mode", "cors")
+		req.Header.Set("sec-fetch-site", "same-origin")
+		req.Header.Set("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+		req.Header.Set("x-requested-with", "XMLHttpRequest")
+		resp, err := client.Do(req)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer resp.Body.Close()
+
+		bodyBytes, err := io.ReadAll(resp.Body)
+
+		pageJson := struct {
+			CategoryProducts string `json:"category_products"`
+			CatalogLeftnav   string `json:"catalog_leftnav"`
+			PageMainTitle    string `json:"page_main_title"`
+			UpdatedUrl       string `json:"updated_url"`
+		}{}
+
+		err = json.Unmarshal(bodyBytes, &pageJson)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		doc := soup.HTMLParse(pageJson.CategoryProducts)
+
+		productsLink := goterators.Map(doc.FindAll("a", "class", "product-item-link"), func(item soup.Root) string {
+			return item.Attrs()["href"]
+		})
+
+		fmt.Printf("%s | page: %d | products: %d\n", category.url, page, len(productsLink))
+
+		allProductsLink = append(allProductsLink, productsLink...)
+
+		page += 1
+	}
 }
 
 func main() {
 	resp, err := soup.Get(BASE_URL)
 
 	if err != nil {
-		os.Exit(1)
+		log.Fatal(err)
 	}
 
 	doc := soup.HTMLParse(resp)
-	pokeCards := doc.FindAll("div", "class", "infocard")
 
-	var allPokemons []Pokemon
+	allCategories := getCategories(doc)
 
-	for _, card := range pokeCards {
-		pokeId := card.Find("span", "class", "infocard-lg-data").Find("small").Text()
-		pokeName := card.Find("a", "class", "ent-name").Text()
-		pokeTypes := goterators.Map(card.FindAll("a", "class", "itype"), func(item soup.Root) string {
-			return item.Text()
-		})
-
-		pokeImage := card.Find("img", "class", "img-sprite").Attrs()["src"]
-
-		pokemon := Pokemon{
-			id:    pokeId,
-			name:  pokeName,
-			types: pokeTypes,
-			image: pokeImage,
-		}
-
-		allPokemons = append(allPokemons, pokemon)
-	}
-
-	f, err := os.Create("output_pokemons.csv")
-	defer f.Close()
-
-	csvWriter := csv.NewWriter(f)
-	defer csvWriter.Flush()
-
-	for _, pokemon := range allPokemons {
-		row := []string{pokemon.id, pokemon.name, pokemon.image, strings.Join(pokemon.types, "|")}
-		if err := csvWriter.Write(row); err != nil {
-			log.Fatalln("error writing record to file", err)
-		}
+	for _, category := range allCategories {
+		allProductsLink := getProductsLinkFromCategory(category)
+		fmt.Println(allProductsLink)
+		break
 	}
 }
